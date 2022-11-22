2267e4e407560e23f56779b0e5bad8fd9047dd32
junkor-1011 <tomoyukizxy@gmail.com>
2022-11-23 00:15:24 +0900

    chore: update Lambda Runtime(16 -> 18)

--- lib/prisma-lambda-app2-stack.ts
+++ lib/prisma-lambda-app2-stack.ts
@@ -35,7 +35,7 @@ export class PrismaLambdaApp2Stack extends Stack {
     const fastifyAppLambda = new NodejsFunction(this, 'FastifyAppLambda', {
       entry: 'packages/fastify-app/lambda.ts',
       handler: 'handler',
-      runtime: lambda.Runtime.NODEJS_16_X,
+      runtime: lambda.Runtime.NODEJS_18_X,
       bundling: {
         sourceMap: true,
         minify: true,
